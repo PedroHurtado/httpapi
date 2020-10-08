a8e272c1d72b7e48b38381caaf317d723e8db7a9
PedroHurtado <pedrohurt@gmail.com>
2020-10-08 16:11:58 +0200

    add strategies ioccontainer

--- src/ioccontainer.js
+++ src/ioccontainer.js
@@ -1,19 +1,33 @@
 //TODO estrategies singleton,value,factory,instance,etc...
-import {validate} from './validate.js'
-export class IocContainer{
-    constructor(){
+import { validate } from './validate.js'
+
+export singleton = function (...dependecies) {
+    return () => { }
+}
+
+export instance = function (...dependecies) {
+    return () => { }
+}
+export value = function (...dependecies) {
+    return () => { }
+}
+export instance = function(...dependecies){
+    return ()=>{}
+ }
+export class IocContainer {
+    constructor() {
         this._dependencies = new Map();
         this._caches = new Map();
     }
-    set(key,strategy){
-        validate(key,'key is required');
-        validate(strategy,'strategy is required');
-        this._dependencies.set(key,strategy)
+    set(key, strategy) {
+        validate(key, 'key is required');
+        validate(strategy, 'strategy is required');
+        this._dependencies.set(key, strategy)
     }
-    get(key){
-        validate(key,'key is required');
+    get(key) {
+        validate(key, 'key is required');
         const strategy = this._dependencies.get(key)
-        if(!strategy){
+        if (!strategy) {
             validate(`${key} dependecies is not defined`)
         }
         return strategy.bind(this)();
